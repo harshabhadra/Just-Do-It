3b392b9006fdf9c1eee7e30928d3b6ab17c7c0e3
harshabhadra <harshasharkey@gmail.com>
2019-08-01 16:07:44 +0530

    Add Task Report Details

---
+++ app/src/main/java/com/example/android/justdoit/Dao/CompletedDao.java
@@ -0,0 +1,27 @@
+package com.example.android.justdoit.Dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.android.justdoit.Model.CompletedTask;
+
+import java.util.List;
+
+@Dao
+public interface CompletedDao {
+
+    @Insert
+    public void insertTask(CompletedTask task);
+
+    @Delete
+    public void deleteTask(CompletedTask task);
+
+    @Query("DELETE from completed_task")
+    public void deleteAll();
+
+    @Query("SELECT * from completed_task ORDER BY id ASC")
+    LiveData<List<CompletedTask>> getAllTask();
+}
